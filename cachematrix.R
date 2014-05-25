761df7a8f113fc2cd13896c73af08cc5180b6a9b
Eric Kristoff <kristoe@yahoo.com>
2014-05-25 12:40:05 -0500

    working with 2x2's, still using m

--- cachematrix.R
+++ cachematrix.R
@@ -7,7 +7,21 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(toSolve) {
+    m <<- toSolve
+  }
+  getInverse <- function() {
+    m
+  }
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
@@ -15,4 +29,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-}
+  m <- x$getInverse()
+  if(!is.null(m)) {
+    message("getting cached matrix data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setInverse(m)
+  m
+}  
